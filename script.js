de126d4044a3d60e615a3a68fbe4ba3dcd67397a
karan punamsing bainade <119426472+Karan-Bainade7@users.noreply.github.com>
2024-12-16 18:27:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//operator is used to perform some operation on data called operation.
+//1. Arithmaatic Operators
+let a=2;
+let b=3;
+
+console.log("additions of a and b= ",a+b);
+console.log("substraction of a and b= ",a-b);
+console.log("multiplications of a and b=",a*b);
+console.log("expoentiation of a and b= ",a**b);
+console.log("division of a and b= ",a/b);
+console.log("modules of a and b= ",a%b);
+console.log("increments of a and b= ",a + + b);
+console.log("decrements of a and b= ",a- -b);
+
+
+//assingment operators
+let x=2;
+let y=3;
+x+=4;
+y*=15;
+
+console.log("output is= ",x);
+console.log("output is =",y);
+
+
+//comparison operators
+let n=5;
+let m=10;
+console.log(n<m);
+console.log(n>m);
+console.log(n==m);
+console.log(n!==m);
+
+
+
+//logical operators
+let c=6;
+let d=2;
+
+let condition1= c>d;//true
+let condition2= c===6;//true
+console.log(condition1 && condition2);//final answer is true
+console.log(condition1 || condition2);//final answer is true
